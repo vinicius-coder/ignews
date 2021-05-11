b688aa45718d4c189504aa9ab236020b241c7f24
Avonts <vinialves022@gmail.com>
2021-05-11 14:38:31 -0300

    Add Global.scss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
-import styles from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Olá mundo
-    </h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>
+        Teste
+      </h1>
+    </>
   )
 }
