b6dd5ecb8c6f7d64b9eea3d77e2c0b041f1acfaa
Avonts <vinialves022@gmail.com>
2021-05-12 00:04:21 -0300

    Add social login with next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,16 +1,21 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi'
 
+import { signIn, signOut, useSession } from 'next-auth/client';
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
 
-    const isUserLogged = true;
+    const [session] = useSession();
 
-    return isUserLogged ? (
+    console.log(session?.user.name);
+
+    return session ? (
         <button
             type="button"
             className={styles.signInButton}
+            onClick={()=>signOut()}
         >
             <FaGithub color="#04d361" />
             Diego Fernandes
@@ -20,6 +25,7 @@ export function SignInButton() {
         <button
             type="button"
             className={styles.signInButton}
+            onClick={()=>signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sign in with Github
